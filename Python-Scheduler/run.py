d1ed7f3afb0099639a4952af3086261d9ee9d7bf
Transcendence-Industries <transcendence.inc.io@gmail.com>
2024-05-07 21:47:08 +0200

    Added Python-Scheduler

---
+++ Python-Scheduler/run.py
@@ -0,0 +1,64 @@
+import os
+import json
+import logging
+import subprocess
+from datetime import datetime
+
+LOG_PATH = "./logs"
+SCRIPT_PATH = "./scripts"
+CONFIG_FILE = "./config.json"
+
+
+def send_email(title, conent):
+    # TODO
+    pass
+
+
+def run_script(script, args):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_name = os.path.join(LOG_PATH, f"{script} - {timestamp}.log")
+    logging.info(f"Starting execution of script '{script}'.")
+
+    try:
+        # Build working directory & args
+        work_dir = os.path.join(SCRIPT_PATH, script)
+        logging.debug(f"Executing in directory: {work_dir}")
+        logging.debug(f"Executing with args: {args}")
+
+        # Execute script
+        process = subprocess.Popen(
+            cwd=work_dir,
+            args=args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True)
+
+        # Capture output
+        with open(log_name, "w") as log_file:
+            for line in process.stdout:
+                logging.debug(f"> {line}")
+                log_file.write(line)
+
+        # Wait for termination
+        exit_code = process.wait()
+        logging.info(f"Finished execution of script '{script}' with exit-code {exit_code}.")
+    except Exception as e:
+        exit_code = -1
+        logging.warning(f"Error during execution of '{script}': {str(e)}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Loading configuration from JSON file...")
+    with open(CONFIG_FILE, "r") as file:
+        data = json.load(file)
+
+    print(data)
+
+    logging.info(f"Starting scheduler for {len(data)} scripts.")
+    for script in data:
+        for run in data[script]:
+            run_script(script, run["args"])
+
+    logging.info("Completed scheduler.")
--- README.md
+++ README.md
@@ -9,3 +9,14 @@
     - Use `docker volume create <VOLUME_NAME>` to create a docker volume with the previous coosen name.
     - Use `docker compose up -d` to deploy the container.
     - Use `docker cp <SRC_DIR> <CONTAINER_NAME>:/app` to copy the website's source files to the previous deployed docker container.
+
+## Python Scheduler
+
+> Simple scheduler that runs scripts with given arguments.
+
+- Setup:
+    - Change the marked settings in the `docker-compose.yml` file.
+    - Use `docker volume create <VOLUME_NAME>` to create a docker volume with the previous coosen name.
+    - Use `docker compose up -d` to deploy the container.
+    - Create a `config.json` file for the configuration.
+    - Use `docker cp <CONFIG_PATH> <CONTAINER_NAME>:/app` to copy the config file to the previous deployed docker container.
