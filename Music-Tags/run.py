052d76c70470f5d1cdbe929230f852d8fe5b3cb9
Transcendence-Industries <transcendence.inc.io@gmail.com>
2025-06-07 12:13:37 +0200

    Updated Music-Tags

--- Music-Tags/run.py
+++ Music-Tags/run.py
@@ -1,9 +1,49 @@
 import os
 import logging
-from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, TIT2, TPE1, TALB, ID3NoHeaderError
 
 SOURCE_PATH_MUSIC = str(os.environ["SOURCE_PATH_MUSIC"])
 INCLUDED_DIRS = str(os.environ["INCLUDED_DIRS"]).split(",")
+FORCE_DELETION = str(os.environ["FORCE_DELETION"]) in ("True", "true")
+FORCE_OVERWRITE = str(os.environ["FORCE_OVERWRITE"]) in ("True", "true")
+
+
+def change_track_tags(track_path, tags):
+    basename = os.path.basename(track_path)
+
+    try:
+        audio = ID3(track_path)
+
+        if FORCE_DELETION:
+            del audio["TIT2"]
+            del audio["TPE1"]
+            del audio["TALB"]
+            audio.save()
+            logging.debug(f"Deleted tags for file '{basename}'.")
+
+        audio = ID3(track_path)
+    except ID3NoHeaderError:
+        audio = ID3()
+
+    changed = []
+
+    if "TIT2" not in audio:
+        audio["TIT2"] = TIT2(encoding=3, text=tags["title"])
+        changed.append("title")
+    if "TPE1" not in audio:
+        audio["TPE1"] = TPE1(encoding=3, text=tags["artist"])
+        changed.append("artist")
+    if "TALB" not in audio:
+        audio["TALB"] = TALB(encoding=3, text=tags["album"])
+        changed.append("album")
+
+    if changed or FORCE_OVERWRITE:
+        audio.save(track_path, v2_version=3)
+        logging.debug(
+            f"Set tags '{','.join(changed)}' for file '{basename}'.")
+    else:
+        logging.debug(
+            f"No tags set for file '{basename}', existing tags are '{audio.keys()}'.")
 
 
 def check_and_set_tags(root_path):
@@ -13,33 +53,42 @@ def check_and_set_tags(root_path):
         if os.path.isdir(artist_path):
             for album_dir in os.listdir(artist_path):
                 album_path = os.path.join(artist_path, album_dir)
+                logging.info(f"> Checking directory '{album_path}'...")
 
+                # Tags from directories (artist -> album -> title.mp3)
                 if os.path.isdir(album_path):
                     for track_file in os.listdir(album_path):
                         track_path = os.path.join(album_path, track_file)
 
                         if os.path.isfile(track_path) and track_file.endswith(".mp3"):
                             title = track_file.replace(".mp3", "")
-                            audio = EasyID3(track_path)
-                            changed = []
 
-                            if "artist" not in audio.keys():
-                                audio["artist"] = str(artist_dir)
-                                changed.append("artist")
-                            if "album" not in audio.keys():
-                                audio["album"] = str(album_dir)
-                                changed.append("album")
-                            if "title" not in audio.keys():
-                                audio["title"] = str(title)
-                                changed.append("title")
+                            change_track_tags(track_path, {
+                                "artist": str(artist_dir),
+                                "album": str(album_dir),
+                                "title": str(title)
+                            })
+                else:  # Tags from filename (artist -> album - title.mp3)
+                    """
+                    for track_file in os.listdir(album_path):
+                        track_path = os.path.join(album_path, track_file)
 
-                            if changed:
-                                audio.save()
-                                logging.info(
-                                    f"Set tags '{','.join(changed)}' for file '{track_path}'.")
-                            else:
-                                logging.info(
-                                    f"No tags set for file '{track_path}'.")
+                        if os.path.isfile(track_path) and track_file.endswith(".mp3"):
+                            base = track_file.replace(".mp3", "")
+
+                            try:
+                                album = base.split(" - ")[0]
+                                title = base.split(" - ")[1]
+                            except:
+                                continue
+
+                            change_track_tags(track_path, {
+                                "artist": str(artist_dir),
+                                "album": str(album),
+                                "title": str(title)
+                            })
+                    """
+                    pass
 
 
 if __name__ == "__main__":
